13132188aee38e7cb704198666957565678c5eda
Fausto Figueroa <faustofi1262@yahoo.com>
2025-05-23 13:16:10 -0500

    Primer commit del proyecto SICOP

---
+++ run.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+
+@app.route('/')
+def index():
+    return "SICOP funcionando en Render!"
+
+if __name__ == '__main__':
+    app.run()
