b59b50770ec0c496f4ecbb010df10a9652576c56
Fausto Figueroa <faustofi1262@yahoo.com>
2025-05-24 22:00:22 -0500

    Commit limpio sin .env ni secretos

--- app/auth.py
+++ app/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, session, url_for
+from flask import Blueprint, render_template, request, redirect, session
 import psycopg2
 import os
 
