83dde3ed757f9a2d2e87bfe25999ab69993e05f8
Fausto Figueroa <faustofi1262@yahoo.com>
2025-05-24 22:45:56 -0500

    Removido archivo .env del historial

--- run.py
+++ run.py
@@ -1,14 +1,2 @@
-from flask import Flask
-from dotenv import load_dotenv
 from app import create_app
-import os
 app = create_app()
-load_dotenv()
-app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-@app.route('/')
-def index():
-    return "SICOP funcionando en Render!"
-
-if __name__ == '__main__':
-    app.run()
