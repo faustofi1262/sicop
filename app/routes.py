9c68f3b976ced417e465b735cd0981621ced6ac8
Fausto Figueroa <faustofi1262@yahoo.com>
2025-06-07 23:42:47 -0500

    Agrego reporte de cate

--- app/routes.py
+++ app/routes.py
@@ -321,10 +321,7 @@ def editar_tarea(id):
         return redirect('/admin/tareas')
 
     conn.close()
-    return render_template('editar_tarea.html',
-                           requerimientos=requerimientos,
-                           tarea=tarea,
-                           tipos_proceso=tipos_proceso)
+    return render_template('editar_tarea.html', requerimientos=requerimientos, tarea=tarea, tipos_proceso=tipos_proceso)
 @main.route('/convertir_a_letras')
 def convertir_a_letras():
     valor = float(request.args.get("valor", 0))
